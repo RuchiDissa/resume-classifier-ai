02554abd70e5193260e5c87967e0fd897d997181
Ruchira dissanayaka <95704401+RuchiDissa@users.noreply.github.com>
2025-07-17 14:19:35 +0300

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,17 +1,54 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 
 auth = Blueprint('auth', __name__)
+
+# Temporary in-memory user storage
+users = {}
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        username = request.form['user']
+        password = request.form['password']
 
-        return redirect(url_for('routes.home'))
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            flash('Logged in successfully', 'success')
+            return redirect(url_for('routes.dashboard'))
+        else:
+            flash('Invalid credentials', 'danger')
+            return redirect(url_for('auth.login'))
+
     return render_template('login.html')
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
+        username = request.form['user']
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        confirm = request.form['confirm_Password']
 
+        if password != confirm:
+            flash("Passwords do not match", "danger")
+            return redirect(url_for('auth.register'))
+
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash("Username already exists", "danger")
+            return redirect(url_for('auth.register'))
+
+        hashed_pw = generate_password_hash(password, method='sha256')
+        new_user = User(username=username, name=name, email=email, password=hashed_pw)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash("Registered successfully", "success")
         return redirect(url_for('auth.login'))
+
     return render_template('register.html')
