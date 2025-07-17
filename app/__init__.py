63686bfcc53b89793f6020d322e60889c9d8f7ad
Ruchira dissanayaka <95704401+RuchiDissa@users.noreply.github.com>
2025-07-17 14:19:13 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
-from .routes import routes
-from .auth import auth
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = '5e36tk37'
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    from .routes import routes
+    from .auth import auth
 
     app.register_blueprint(routes)
     app.register_blueprint(auth)
