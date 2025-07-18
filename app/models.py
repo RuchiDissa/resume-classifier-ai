f317ea77760757caec91e43fa8a63113d13e0f4f
Ruchira dissanayaka <95704401+RuchiDissa@users.noreply.github.com>
2025-07-18 14:50:44 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -6,4 +6,5 @@ class User(db.Model):
     name = db.Column(db.String(150), nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
-    role = db.Column(db.String(50), default='none')  # 🔑 Add this line
+    role = db.Column(db.String(50), default='none') 
+    blocked = db.Column(db.Boolean, default=False)
