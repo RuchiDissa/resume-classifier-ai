2903f5b88437db9642cbff1d6eec4b3275be9e9d
Ruchira dissanayaka <95704401+RuchiDissa@users.noreply.github.com>
2025-07-23 14:01:31 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -9,20 +9,19 @@ def create_app():
     app = Flask(__name__)
     app.secret_key = '5e36tk37'
 
-    
+    app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static', 'uploads')
+    app.config['ALLOWED_EXTENSIONS'] = {'pdf', 'docx', 'txt'}
+
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
-    
+
     from .routes import routes
     from .auth import auth
 
     app.register_blueprint(routes)
     app.register_blueprint(auth)
-
-    app.config['UPLOAD_FOLDER']=os.path.join(os.getcwd(),'uploads')
-    app.config['ALLOWED_EXTENSIONS']={'pdf','docx','txt'}
 
     return app
