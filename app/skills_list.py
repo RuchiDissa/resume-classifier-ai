edaaae37829de75e1b1ef55fbadc4da41a241a63
Ruchira dissanayaka <95704401+RuchiDissa@users.noreply.github.com>
2025-07-28 11:31:55 +0300

    Create skills_list.py

---
+++ app/skills_list.py
@@ -0,0 +1,6 @@
+SKILLS = [
+    "Python", "Java", "C++", "SQL", "HTML", "CSS",
+    "JavaScript", "Machine Learning", "Data Analysis",
+    "Flask", "Django", "Git", "React", "Tensorflow",
+    "Keras", "Linux", "AWS"
+]
